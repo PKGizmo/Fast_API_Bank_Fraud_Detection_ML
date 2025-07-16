af429d681658536cbd4c4b1a1faf3254ea1cd6dc
Piotr <pkarwack@gmail.com>
2025-07-16 23:16:46 +0200

    Chore: Structuring, created .env files

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,8 +4,3 @@ app = FastAPI(
     title="Fast API Bank Fraud Detection ML",
     description="Fully featured banking API built with FastAPI",
 )
-
-
-@app.get("/")
-def home():
-    return {"message": "Welcome to the NextGen Bank API!"}
