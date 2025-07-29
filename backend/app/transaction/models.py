6846c745565b8b350612a6336b8cef2a90cde2c7
Piotr <pkarwack@gmail.com>
2025-07-29 15:06:08 +0200

    Feat.:  add idempotency key model, transaction schema and utils

--- backend/app/transaction/models.py
+++ backend/app/transaction/models.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import datetime, timezone
 from typing import TYPE_CHECKING
 
-from sqlmodel import Field, Column, Relationship
+from sqlmodel import Field, Column, Relationship, SQLModel
 from sqlalchemy.dialects import postgresql as pg
 from sqlalchemy import text, func
 
@@ -82,4 +82,45 @@ class Transaction(TransactionBaseSchema, table=True):
     processor: "User" = Relationship(
         back_populates="processed_transactions",
         sa_relationship_kwargs={"foreign_keys": "Transaction.processed_by"},
+    )
+
+
+class IdempotencyKey(SQLModel, table=True):
+    id: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID(as_uuid=True),
+            primary_key=True,
+        ),
+        default_factory=uuid.uuid4,
+    )
+
+    key: str = Field(index=True, unique=True)
+    user_id: uuid.UUID = Field(foreign_key="user.id")
+
+    # This will help to identify which API call the key is related to
+    endpoint: str
+
+    # Will store the response code of the original request
+    # so that the same response can be returned after duplicated call
+    response_code: int
+
+    # Will store the same response in the database
+    # so that it can be returned after duplicated call without reprocessing the request
+    response_body: dict = Field(sa_column=Column(JSONB))
+
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column=Column(
+            pg.TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+        ),
+    )
+    # Time at which Idempotency Key expires so that it cannot be reused indefinitely
+    expires_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column=Column(
+            pg.TIMESTAMP(timezone=True),
+            nullable=False,
+        ),
     )
