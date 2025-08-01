41c85135f2397f697018a4eba588207550456cef
Piotr <pkarwack@gmail.com>
2025-08-01 13:10:14 +0200

    Feat.: add virtual card enums, schema and models.

---
+++ backend/app/virtual_card/enums.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+
+class VirtualCardStatusEnum(str, Enum):
+    Active = "acvite"
+    Inactive = "inactive"
+    Pending = "pending"
+    Blocked = "blocked"
+    Expired = "expired"
+
+
+class VirtualCardTypeEnum(str, Enum):
+    Debit = "debit"
+    Credit = "credit"
+
+
+class VirtualCardBrandEnum(str, Enum):
+    Visa = "visa"
+
+
+class VirtualCardCurrencyEnum(str, Enum):
+    USD = "USD"
+    EUR = "EUR"
+    GBP = "GBP"
+    KES = "KES"
+    PLN = "PLN"
+
+
+class CardBlockReasonEnum(str, Enum):
+    Lost = "lost"
+    Stolen = "stolen"
+    Suspicious_Activity = "suspicious_activity"
+    Customer_Request = "customer_request"
